5763eb459ea47aa2734db98d72597adafeab0b38
kywdavis <37345117+kywdavis@users.noreply.github.com>
2018-10-11 00:19:31 -0700

    Added requested files
    Added python scripts requested in homework assignment 1

---
+++ check_in_solution.py
@@ -0,0 +1,23 @@
+# include the Numpy library
+import numpy as np
+
+
+#define the main() function
+def main():
+    
+    i = 0
+    x = 119.0
+    
+    for i in range(120):
+        if((i%2)==0):
+            x += 3.
+        else:
+            x -= 5.
+            
+    s = "%3.2e" % x
+    
+    print(s)
+    
+#now the rest of the program continues
+if __name__ == "__main__":
+    main()
